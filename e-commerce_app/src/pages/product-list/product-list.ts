b92eb9e80b669dbf9153f947442b47ba9cf7288b
Stark <mrcs.stark@gmail.com>
2018-12-16 14:55:17 -0300

    Implementa listagem de produtos no app, resolve problema de cors no Laravel

---
+++ e-commerce_app/src/pages/product-list/product-list.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+/**
+ * Generated class for the ProductListPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-product-list',
+  templateUrl: 'product-list.html',
+})
+export class ProductListPage {
+
+  public products = [];
+
+  constructor(
+      public navCtrl: NavController,
+      public navParams: NavParams,
+      public http: Http
+  ) { }
+
+  ionViewDidLoad() {
+    this.http.get('http://localhost:8000/api/products')
+        .toPromise().then((response)=> {
+        this.products = response.json();
+    });
+  }
+
+}
